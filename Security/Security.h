027db53a05dd6006c45be97b4124eae19e3c875b
Yonghui Chen <chyhfj@gmail.com>
2015-04-11 16:27:35 +0800

    Initial Commit

---
+++ Security/Security.h
@@ -0,0 +1,19 @@
+//
+//  Security.h
+//  Security
+//
+//  Created by Chen Yonghui on 4/11/15.
+//  Copyright (c) 2015 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Security.
+FOUNDATION_EXPORT double SecurityVersionNumber;
+
+//! Project version string for Security.
+FOUNDATION_EXPORT const unsigned char SecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Security/PublicHeader.h>
+
+
